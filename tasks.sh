47a229666070ab621aafe4101518260359633cb3
Marc Amorós <marc.amoros@tum.de>
2024-09-23 15:17:49 +0200

    Adding task.sh script to repo

---
+++ tasks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# python src/pruning_experiments.py --xsize 104 --ysize 52 --convnext_mult 1 --prune_times 0 --epochs 1000 --seed -1
+# python src/pruning_experiments.py --xsize 104 --ysize 52 --convnext_mult 2 --prune_times 8 --prune_perc 0.085 --prune_type L2 --epochs 400 --seed -1
+# python src/pruning_experiments.py --xsize 104 --ysize 52 --convnext_mult 3 --prune_times 11 --prune_perc 0.1 --prune_type L2 --epochs 400 --seed -1
+# python src/pruning_experiments.py --xsize 104 --ysize 52 --convnext_mult 4 --prune_times 14 --prune_perc 0.1 --prune_type L2 --epochs 400 --seed -1
+
